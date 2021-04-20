8bde69b5e5c25abf5f9071e4bd51b53ec61924ce
artem <mashkov-artem@mail.ru>
2021-04-20 15:26:02 +0600

    дз 6

---
+++ lesson_6_ООП/hw lesson_6/hw6_4.py
@@ -0,0 +1,50 @@
+# 4. Реализуйте базовый класс Car.
+# У данного класса должны быть следующие атрибуты: speed, color, name, is_police (булево).
+# А также методы: go, stop, turn(direction),
+# которые должны сообщать, что машина поехала, остановилась, повернула (куда).
+# Опишите несколько дочерних классов: TownCar, SportCar, WorkCar, PoliceCar.
+# Добавьте в базовый класс метод show_speed, который должен показывать текущую скорость автомобиля.
+# Для классов TownCar и WorkCar переопределите метод show_speed.
+# При значении скорости свыше 60 (TownCar) и 40 (WorkCar)
+# должно выводиться сообщение о превышении скорости.
+# Создайте экземпляры классов, передайте значения атрибутов.
+# Выполните доступ к атрибутам, выведите результат.
+# Выполните вызов методов и также покажите результат.
+
+class Car:
+    def __init__(self, speed, color, name, is_police = False):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = is_police
+
+    def go(self):
+        print('машина приехала')
+
+    def stop(self):
+        print('машина остановилась')
+
+    def turn(self):
+        print('машина повернула')
+
+class WorkCar(Car):
+    def show_speed(self):
+        if self.speed > 40:
+            print(f'вы превысили скорость, скорость автомобиля: {self.speed}' )
+        else:
+            print(f'скорость автомобиля: {self.speed}')
+
+class TownCar(Car):
+    def show_speed(self):
+        if self.speed > 60:
+            print(f'вы превысили скорость, скорость автомобиля: {self.speed}' )
+        else:
+            print(f'скорость автомобиля: {self.speed}')
+
+kamaz = WorkCar(50, 'red', 'Kamaz', False)
+print(kamaz.speed, kamaz.name, kamaz.color, kamaz.is_police)
+print(kamaz.show_speed())
+
+taxi = TownCar(60, 'yellow', 'Ford', False)
+print(taxi.color, taxi.speed, taxi.name, taxi.is_police)
+print(taxi.show_speed())
